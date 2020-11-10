99f4beaf9f2d453b377b7caafc84e3fe282e4058
Giuseppe Perna <giuseppeperna.cg@gmail.com>
2020-11-10 15:52:20 +0100

    JSnacks
    completed

--- jSnack-4/main.js
+++ jSnack-4/main.js
@@ -4,18 +4,43 @@ Ogni squadra avrà diverse proprietà:
 nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre
 saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà:
+punti fatti e falli subiti
 @author Giuseppe Perna <giuseppeperna.cg@gmail.com>
 */
 
 // RUN CODE AFTER DOM
 $(document).ready( function() {
 
-  // CONSTANT VARIABLES
-  var TEAMS_NUMBER = 4;
+  // Array di oggetti team
+  var teams = [
+    {
+      'nome': "Napoli",
+      'punti': 0,
+      'falli subiti': 0
+    },
+    {
+      'nome': "Genoa",
+      'punti': 0,
+      'falli subiti': 0
+    },
+    {
+      'nome': "Spal",
+      'punti': 0,
+      'falli subiti': 0
+    },
+    {
+      'nome': "Benevento",
+      'punti': 0,
+      'falli subiti': 0
+    }
+  ];
 
-  var teams = [];
-  for (var i = 0; i < TEAMS_NUMBER; i++) {
-    
+  /*Generazione numeri random per le proprietà "punti" e "falli subiti"
+  degli oggetti team*/
+  for (var i = 0; i < teams.length; i++){
+    teams[i].punti = Math.floor(Math.random() * 30);
+    teams[i]['falli subiti'] = Math.floor(Math.random() * 35);
   }
-
+  console.log(teams);
 })
